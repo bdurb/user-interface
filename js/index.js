1003dec09fa7a9833bab3d4aeca2a1c12e9e4d00
bdurb <brian@rpmgreenville.com>
2018-06-06 10:29:54 -0700

    working on styling and js functionality for menu dropdown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+let overlay = document.querySelector('.overlay');
+let menuButton = document.querySelector('.nav-btn');
+let closeButton = document.querySelector('.close-btn');
+
+menuButton.addEventListener('click', function() {
+  overlay.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function() {
+  overlay.style.display = 'none';
+});
