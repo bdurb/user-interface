1e6bee8a924d8e99dc6f8e16a60761285ef21cfa
bdurb <brian@rpmgreenville.com>
2018-06-06 15:47:18 -0700

    added scroll event listner for nav background color change

--- js/index.js
+++ js/index.js
@@ -11,3 +11,13 @@ menuButton.addEventListener('click', function() {
 closeButton.addEventListener('click', function() {
   overlay.style.display = 'none';
 });
+
+
+const showNav = () => {
+    nav.style.position = "fixed";
+    nav.style.top = 0;
+    nav.style.backgroundColor = "#9DA7B1";
+    nav.style.opacity = 0.95;
+  };
+  const nav = document.querySelector(".navigation");
+  window.addEventListener("scroll", showNav);
